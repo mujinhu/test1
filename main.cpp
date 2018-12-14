bba4154ef517004b4f42775a514cced9dc7a03f1
mujinhu <45555840+mujinhu@users.noreply.github.com>
2018-12-14 09:50:10 +0800

    Update main.cpp
    Use the main() function to test the new functions in every task.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,17 @@ int main(int argc, char **argv){
 	Node *head;
 	//make the head node
 	head=makeNode(0.0,0.0, 0);
+	//growTree(head);
+	//growTree(head);
+	//makeChildren(head->child[0]);
+	//makeChildren(head->child[1]);
+	//do{
+	//	flag(head);
+	//	adapta = adaptr = 0;
+	//	adapt(head);
+	//	printf("I have added %d nodes and removed %d nodes\n",adapta,adaptr);
+	//}while(adapta + adaptr != 0);
+	//destroyTree(head);
 	//make a tree
 	makeChildren(head); 
 	writeTree(head);
